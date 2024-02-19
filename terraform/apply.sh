edaea6e325e20581658840dd526df0f382091760
Ivo Calado <ivo.calado@hover.to>
2024-02-19 00:53:27 -0300

    Fix typo

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Executing terraform/apply.sh"
-echo "Execuring tflocal init"
+echo "Executing tflocal init"
 tflocal init
 echo "Executing tflocal apply -auto-approve"
 tflocal apply -auto-approve
